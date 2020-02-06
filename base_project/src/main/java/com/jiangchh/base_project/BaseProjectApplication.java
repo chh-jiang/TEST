ca452bf2055465cf6583d00e6e4c3c464cc0cd29
chh-jiang <jch429701725@sina.com>
2020-02-06 15:54:00 +0800

    init

---
+++ base_project/src/main/java/com/jiangchh/base_project/BaseProjectApplication.java
@@ -0,0 +1,26 @@
+package com.jiangchh.base_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+
+@SpringBootApplication
+public class BaseProjectApplication {
+
+    public void test  {
+        System.out.println();
+        
+    }
+
+    public static void main(String[] args) {
+        int i=123;
+        System.out.println("i = " + i);
+
+        for (int j = 0; j < ; j++) {
+            
+        }
+    }
+
+}
+    private static final int  = 909;
